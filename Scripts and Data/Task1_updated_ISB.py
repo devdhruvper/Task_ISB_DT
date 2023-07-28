a2e16c820cc7f13ddd8ecb55b8dc8f62a5df8b5a
Dhruv Talan <121749576+devdhruvper@users.noreply.github.com>
2023-07-28 15:05:55 +0530

    Update Task1_updated_ISB.py
    updating

--- Scripts and Data/Task1_updated_ISB.py
+++ Scripts and Data/Task1_updated_ISB.py
@@ -1,4 +1,16 @@
+#Task1 - Dhruv Talan
+#defining the function we will need to convert our date into the following fromat of dd-mm-yyy
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
+def convert_suitably(data_str):
+ date_obj = datetime.strptime(data_str, "%d/%m/%Y")
+ new_date_str = date_obj.strftime("%d-%m-%Y")
+ return new_date_str
+
+data_final_list=[]
 for year in range(2008,2024):
 
     URL = f'https://www.teaboard.gov.in/WEEKLYPRICES/{year}'
@@ -32,6 +44,17 @@ for year in range(2008,2024):
 
     rows = [cell[i:i + lencn] for i in range(0, lencell, lencn)]
 
-    for row in rows:
-        r = pd.DataFrame([row], columns=df.columns)
-        df = df.append(r, ignore_index=True)
+
+    r = pd.DataFrame(rows, columns=df.columns)
+    df=pd.concat([df,r],ignore_index=True)
+    df.reset_index(drop=True, inplace=True)
+
+    df=df.drop(columns=['Tea Serve'],axis=0)
+    df=pd.melt(df,id_vars=['Week Ending/Date'],var_name="location",value_name="average_price")
+    df.rename(columns={'Week Ending/Date':'week'},inplace=True)
+    data_final_list.append(df)
+df_final_tables = pd.concat(data_final_list)
+df_final_tables.reset_index(drop=True, inplace=True)#making datframe
+print(df_final_tables) #the complete dataframe for all years
+df_final_tables['week']=df_final_tables['week'].apply(convert_suitably) #applying the function to entire column
+df_final_tables.to_csv('Data of WEEKLY AVERAGE PRICES OF TOTAL TEA SOLD AT INDIAN AUCTION 2008-2023.csv',index=False) #converting it to csv
